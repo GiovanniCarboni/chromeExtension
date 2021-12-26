cd1d8232fbe3d127b83b4b3a9c414d9e2c0126c3
Giovanni Carboni <giovanni.carboni.1997@gmail.com>
2021-12-27 01:37:57 +0200

    work of the day

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,19 @@
-let inputBtn = document.getElementById("input-btn");
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#ul-el");
+
+
+let myLeads = [];
 
 inputBtn.addEventListener("click", function() {
-	console.log("Button clicked!")
-})
+	myLeads.push(inputEl.value);
+	renderLeads();
+});
+
+function renderLeads() {
+	let listItems = "";
+	for (let i = 0; i < myLeads.length; i++) {
+		listItems += `<li>${myLeads[i]}</li>`;
+	}
+	ulEl.innerHTML = listItems;
+};
