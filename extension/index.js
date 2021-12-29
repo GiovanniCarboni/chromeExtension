250c1fe3850d88bada9848e5f5479dc093ad1386
Giovanni Carboni <giovanni.carboni.1997@gmail.com>
2021-12-29 13:14:18 +0200

    app ready to be deployed

--- extension/index.js
+++ extension/index.js
@@ -8,12 +8,20 @@ let myLeads = [];
 inputBtn.addEventListener("click", function() {
 	myLeads.push(inputEl.value);
 	renderLeads();
+	inputEl.value = " ";
 });
 
 function renderLeads() {
 	let listItems = "";
 	for (let i = 0; i < myLeads.length; i++) {
-		listItems += `<li>${myLeads[i]}</li>`;
-	}
+		listItems += `
+					<li>
+						<a href="${myLeads[i]}" target="_blank">
+							${myLeads[i]}
+						</a>
+					</li>
+					`;
+	};
 	ulEl.innerHTML = listItems;
 };
+
