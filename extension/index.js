067725b79d6de60b1521b13f118172c098a864c5
Giovanni Carboni <giovanni.carboni.1997@gmail.com>
2021-12-29 17:02:40 +0200

    yay

--- extension/index.js
+++ extension/index.js
@@ -1,15 +1,30 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.querySelector("#input-el");
+const clearBtn = document.getElementById("clear-btn");
 const ulEl = document.querySelector("#ul-el");
 
 
 let myLeads = [];
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage;
+	renderLeads();
+}
+
 inputBtn.addEventListener("click", function() {
 	myLeads.push(inputEl.value);
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
 	renderLeads();
 	inputEl.value = " ";
+	console.log(localStorage.getItem("myLeads"));
 });
+
+clearBtn.addEventListener("click", function() {
+	localStorage.clear();
+	myLeads = [];
+	renderLeads();
+})
 
 function renderLeads() {
 	let listItems = "";
