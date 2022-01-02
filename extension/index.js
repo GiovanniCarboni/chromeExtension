1f777c1bd2f655b08d343568de9ac0aa8a044b4d
Giovanni Carboni <giovanni.carboni.1997@gmail.com>
2022-01-02 22:08:34 +0200

    end of the extension

--- extension/index.js
+++ extension/index.js
@@ -2,41 +2,57 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.querySelector("#input-el");
 const clearBtn = document.getElementById("clear-btn");
 const ulEl = document.querySelector("#ul-el");
-
+const tabBtn = document.querySelector("#tab-btn");
+const showBtn = document.getElementById("show-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 let myLeads = [];
+let oldLeads = null; // do you wanna show the old leads? make this button
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 if (leadsFromLocalStorage) {
 	myLeads = leadsFromLocalStorage;
-	renderLeads();
+	render(myLeads);
 }
 
-inputBtn.addEventListener("click", function() {
-	myLeads.push(inputEl.value);
-	localStorage.setItem("myLeads", JSON.stringify(myLeads));
-	renderLeads();
-	inputEl.value = " ";
-	console.log(localStorage.getItem("myLeads"));
-});
-
-clearBtn.addEventListener("click", function() {
-	localStorage.clear();
-	myLeads = [];
-	renderLeads();
-})
-
-function renderLeads() {
+function render(arr) {
 	let listItems = "";
-	for (let i = 0; i < myLeads.length; i++) {
+	for (let i = 0; i < arr.length; i++) {
 		listItems += `
 					<li>
-						<a href="${myLeads[i]}" target="_blank">
-							${myLeads[i]}
+						<a href="${arr[i]}" target="_blank">
+							${arr[i]}
 						</a>
 					</li>
 					`;
 	};
 	ulEl.innerHTML = listItems;
 };
+
+inputBtn.addEventListener("click", function() {
+	myLeads.push(inputEl.value);
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
+	render(myLeads);
+	inputEl.value = " ";
+	console.log(localStorage.getItem("myLeads"));
+});
+
+tabBtn.addEventListener("click", function() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		myLeads.push(tabs[0].url);
+		localStorage.setItem("myLeads", JSON.stringify(myLeads));
+		render(myLeads);
+	});
+});
+
+clearBtn.addEventListener("click", function() {
+	if (myLeads.length > 0) {oldLeads = myLeads};
+	localStorage.setItem("oldLeads", JSON.stringify(oldLeads));
+	localStorage.clear("myLeads");
+	myLeads = [];
+	render(myLeads);
+});
+
+showBtn.addEventListener("click", function() {
+	render(oldLeads);
+});
 
